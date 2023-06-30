18614e7deb677774bdb1813878131715549472a6
singhashwani003 <123533223+singhashwani003@users.noreply.github.com>
2023-06-30 15:56:45 +0530

    All the updation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import HomeOne from "./Components/Home/HomeOne";
+
 import HomeThree from "./Components/Home/HomeThree";
-import HomeTwo from "./Components/Home/HomeTwo";
 import ErrorPage from "./Components/Pages/404Page";
 import AboutOne from "./Components/Pages/AboutOne";
 import AboutTwo from "./Components/Pages/AboutTwo";
-import BLogStandard from "./Components/Pages/BLogStandard";
 import ContactUs from "./Components/Pages/ContactUs";
 import Courses from "./Components/Pages/Courses";
-import CourseSideBar from "./Components/Pages/CourseSideBar";
 import Event from "./Components/Pages/Event";
 import EventSingle from "./Components/Pages/EventSingle";
-import InstructorDetails from "./Components/Pages/InstructorDetails";
-import InstructorOne from "./Components/Pages/InstructorOne";
 import InstructorTwo from "./Components/Pages/InstructorTwo";
 import SingleBlog from "./Components/Pages/SingleBlog";
 import SingleCourse from "./Components/Pages/SingleCourse";
@@ -30,11 +25,6 @@ function App() {
             path="/"
             element={<HomeThree />}
           />
-          <Route path="/react-templates/edumim" element={<HomeOne />} />
-          <Route
-            path="/react-templates/edumim/home-two"
-            element={<HomeTwo />}
-          />
           <Route
             path="/react-templates/edumim/home-three"
             element={<HomeThree />}
@@ -45,16 +35,8 @@ function App() {
             element={<AboutTwo />}
           />
           <Route
-            path="/react-templates/edumim/instructor"
-            element={<InstructorOne />}
-          />
-          <Route
             path="/react-templates/edumim/instructor-two"
             element={<InstructorTwo />}
-          />
-          <Route
-            path="/react-templates/edumim/instructor-details"
-            element={<InstructorDetails />}
           />
           <Route path="/react-templates/edumim/event" element={<Event />} />
           <Route
@@ -71,10 +53,6 @@ function App() {
           />
           <Route path="/react-templates/edumim/error" element={<ErrorPage />} />
           <Route path="/react-templates/edumim/courses" element={<Courses />} />
-          <Route
-            path="/react-templates/edumim/courses-sidebar"
-            element={<CourseSideBar />}
-          />
           <Route
             path="/react-templates/edumim/single-course"
             element={<SingleCourse />}
